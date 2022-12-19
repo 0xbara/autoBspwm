9d159c5646cffab5e3c0eb2522d3e829b8b7183e
Agustin Baranowski <94456392+barambolas@users.noreply.github.com>
2022-12-19 23:20:46 +0000

    Update autoBspwm.sh

--- autoBspwm.sh
+++ autoBspwm.sh
@@ -63,37 +63,37 @@ function main(){
 }
 
 function kali(){
-echo -e "${green}[*]${end} Installing packages..."
-sleep 1
+	echo -e "${green}[*]${end} Installing packages..."
+	sleep 1
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install kitty zsh zsh-autosuggestions zsh-syntax-highlighting bat xclip -y
-sleep 1
+	sudo apt update && sudo apt upgrade -y
+	sudo apt install kitty zsh zsh-autosuggestions zsh-syntax-highlighting bat xclip -y
+	sleep 1
 
-sudo usermod --shell /usr/bin/zsh root
+	sudo usermod --shell /usr/bin/zsh root
 
-usermod --shell /usr/bin/zsh $USER
+	usermod --shell /usr/bin/zsh $USER
 
-sudo rm -rf ~/.config/nvim 2>/dev/null
-sudo rm -rf ~/.config/kitty 2>/dev/null
+	sudo rm -rf ~/.config/nvim 2>/dev/null
+	sudo rm -rf ~/.config/kitty 2>/dev/null
 
-mv files/kitty ~/.config/
-mv files/nvim ~/.config/
-cp -r ~/.config/nvim /root/.config
-sudo mv files/HackFonts/* /usr/share/fonts
+	mv files/kitty ~/.config/
+	mv files/nvim ~/.config/
+	cp -r ~/.config/nvim /root/.config
+	sudo mv files/HackFonts/* /usr/share/fonts
 
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
-sudo echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /root/.zshrc
+	sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
+	sudo echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /root/.zshrc
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-rm -rf ~/.zshrc 
-mv files/.zshrc ~/
-sudo ln -s -f ~/.zshrc /root/.zshrc
+	rm -rf ~/.zshrc 
+	mv files/.zshrc ~/
+	sudo ln -s -f ~/.zshrc /root/.zshrc
 
-wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb 
-sudo dpkg -i lsd_0.23.1_amd64.deb
+	wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb 
+	sudo dpkg -i lsd_0.23.1_amd64.deb
 }
 
 if [ "$(id -u)" == "0" ]; then
